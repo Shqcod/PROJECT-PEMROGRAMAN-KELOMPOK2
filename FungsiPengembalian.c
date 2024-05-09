e267559161b16b745dfeef5f2b6f03bb64ef66b2
Cutrenatha <166110072+Cutrenatha@users.noreply.github.com>
2024-05-09 21:23:55 +0700

    Update

--- FungsiPengembalian.c
+++ FungsiPengembalian.c
@@ -1,7 +1,7 @@
-void kembalikan(struct Buku *buku, unsigned int jumlahBuku, unsigned int id) {
+void pengembalian(struct Buku *buku, unsigned int jumlahBuku, unsigned int id) {
     if (id < jumlahBuku) {
-        if (buku[id].dipinjam) {
-            buku[id].dipinjam = 0;
+        if (buku[id].pinjem) {
+            buku[id].pinjem = 0;
             printf("Buku dengan ID %u telah berhasil dikembalikan.\n", buku[id].idBuku);
         } else {
             printf("Buku dengan ID %u tidak sedang dipinjam.\n", buku[id].idBuku);
@@ -15,6 +15,6 @@ void kembalikan(struct Buku *buku, unsigned int jumlahBuku, unsigned int id) {
 unsigned int id;
 printf("Masukkan ID buku yang ingin dikembalikan: ");
 scanf("%u", &id);
-kembalikan(buku, jumlahBuku, id);
+pengembalian(buku, jumlahBuku, id);
 
 
