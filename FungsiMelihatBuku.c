08316891292e47dab903605a16642075780754c4
yafi <yafihidayatullah123@gmail.com>
2024-05-16 00:12:30 +0700

    update

--- FungsiMelihatBuku.c
+++ FungsiMelihatBuku.c
@@ -1,79 +1,28 @@
-#include <stdio.h>
-#include <string.h>
-
-typedef struct {
-    unsigned int id;
-    char title[48];
-    char author[36];
-    unsigned int page;
-    unsigned int year;
-    unsigned int available;
-} book;
-
-void books_available(book data[]) {
-    FILE *input_fp = fopen("books.txt", "r");
-    int count = 0;
-
-    if (input_fp == NULL) {
-        printf("Error! File doesn't exist\n");
-        return;
-    }
-
-    while (!feof(input_fp)) {
-        fscanf(input_fp, "%u,\"%[^\"]\",%[^,],%u,%u,%u\n", &data[count].id, data[count].title, data[count].author, &data[count].page, &data[count].year, &data[count].available);
-        count++;
-    }
-
-    fclose(input_fp);
-}
-
-int booklist_length() {
-    FILE *input_fp = fopen("books.txt", "r");
-    int count = 0;
-    char line[256];
-
-    if (input_fp == NULL) {
-        printf("Error! File doesn't exist\n");
-        return 0;
-    }
-
-    while (fgets(line, sizeof(line), input_fp) != NULL) {
-        count++;
-    }
-
-    fclose(input_fp);
-    return count;
-}
-
-void table_border(char *option) {
-    if (strcmp(option, "TRUE") == 0) {
-        printf("+------+--------------------------------------------------+-------------------------------------+------------+------------+------------+\n");
+void SimpanDataBukuYangsedangdipinjam(struct Buku *buku, int jumlahBuku) {
+    FILE *file = fopen("pinjaman.txt", "w");
+    if (file != NULL) {
+        for (int i = 0; i < jumlahBuku; i++) {
+            fprintf(file, "%u;%s;%s;%u;%u;%u;%d\n", buku[i].idBuku, buku[i].judul, buku[i].penulis, buku[i].jumlahHalaman, buku[i].tahunTerbit, buku[i].jumlahBukuTersedia, buku[i].dipinjam);
+        }
+        fclose(file);
     } else {
-        printf("+------+--------------------------------------------------+-------------------------------------+------------+------------+------------+\n");
+        printf("Gagal membuka file untuk menyimpan data buku.\n");
     }
 }
 
-void table_row(book data[], int index) {
-    printf("| %-5u | %-47s | %-35s | %-11u | %-11u | %-11u |\n", data[index].id, data[index].title, data[index].author, data[index].page, data[index].year, data[index].available);
-}
-
-int main() {
-    int books_size = booklist_length();
-    book data[books_size];
-    books_available(data);
-
-    printf("\033[1;33m");
-    table_border("TRUE");
-    printf("| %-5s | %-47s | %-35s | %-11s | %-11s | %-11s |\n", "ID", "Title", "Author", "Page", "Year", "Available");
-    table_border("TRUE");
-    printf("\033[0m");
-
-    for (int index = 0; index < books_size; index++) {
-        table_row(data, index);
+void lihatyangsedangdipinjem(struct Buku *buku, unsigned int *jumlahBuku) {
+    FILE *file = fopen("pinjaman.txt", "r");
+    if (file != NULL) {
+        unsigned int idBuku;
+        while (fscanf(file, "%u;", &idBuku[jumlahBuku]) == 1) {
+            printf("ID Buku: %u\n", idBuku[jumlahBuku]);
+        }
+        fclose(file);
+    } else {
+        printf("Gagal membuka file pinjaman.txt\n");
     }
-
-    table_border("FALSE");
-    printf("\033[0m");
-
-    return 0;
 }
+
+printf("Lihat Buku yang sudah di pinjem di file pinjaman.txt");
+SimpanDataBukuYangsedangdipinjam(buku, jumlahBuku);
+lihatyangsedangdipinjem(buku, &jumlahBuku);
